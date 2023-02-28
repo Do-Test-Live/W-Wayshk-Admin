fe1e02b2d29a1c0eaad057d521e6abdbfeda834b
Monoget <monoget2@gmail.com>
2023-02-28 15:49:01 +0600

    update all

---
+++ profile.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+require_once("include/dbController.php");
+$db_handle = new DBController();
+if (!isset($_SESSION['userid'])) {
+    header("Location: index.php");
+}?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Profile | Wayshk Admin</title>
+    <?php include 'include/css.php'; ?>
+</head>
+<body>
+
+<!--*******************
+    Preloader start
+********************-->
+<div id="preloader">
+    <div class="sk-three-bounce">
+        <div class="sk-child sk-bounce1"></div>
+        <div class="sk-child sk-bounce2"></div>
+        <div class="sk-child sk-bounce3"></div>
+    </div>
+</div>
+<!--*******************
+    Preloader end
+********************-->
+
+<!--**********************************
+    Main wrapper start
+***********************************-->
+<div id="main-wrapper">
+
+    <?php include 'include/header.php'; ?>
+
+    <?php include 'include/nav.php'; ?>
+
+    <!--**********************************
+        Content body start
+    ***********************************-->
+    <div class="content-body">
+        <!-- row -->
+        <div class="container-fluid">
+            <!-- Add Order -->
+
+        </div>
+    </div>
+    <!--**********************************
+        Content body end
+    ***********************************-->
+
+    <?php include 'include/footer.php'; ?>
+
+</div>
+<!--**********************************
+    Main wrapper end
+***********************************-->
+
+<?php include 'include/js.php'; ?>
+</body>
+</html>
